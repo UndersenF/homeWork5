87bb57bae638a8b0971c5d86dc578a1a6230b88d
UndersenF <begintolife@mail.ru>
2024-05-17 06:40:39 +0300

    tasks 2, 3, 5,  are corrected

--- src/Main.java
+++ src/Main.java
@@ -12,31 +12,33 @@ public class Main {
 //        task2
         System.out.println();
 
-        clientOS = 1;
         int clientDeviceYear = 2015;
+        clientOS = 1;
+
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения iOS по ссылке.");
-            }
         }
-        if (clientOS == 1) {
+        else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения Android по ссылке.");
-            }
         }
+        else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            System.out.println("Установите облегченную версию приложения iOS по ссылке.");
+            }
+
+        else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            System.out.println("Установите облегченную версию приложения Android по ссылке.");
+        }
+
 
 //        task3
         System.out.println();
 
-        int year = 2025;
+        int year = 2015;
 
         if (year % 400 == 0) {
             System.out.printf("%s год является високосным!\n", year);
-        }
-        else if (year % 100 == 0) {
-            System.out.printf("%s год не является високосным\n", year);
         }
         else if ((year % 4 == 0) && year > 1584) {
             System.out.printf("%s год является високосным!\n", year);
@@ -52,13 +54,13 @@ public class Main {
         int timeOfDelivery = 1;
 //        Можно и добавить объявление переменной timeOfDelivery в каждом результате if-а, но я решил попробовать так
         if (deliveryDistance <= 20) {
-            System.out.println(" Потребуется дней : " + timeOfDelivery);
+            System.out.println("Потребуется дней : " + timeOfDelivery);
         }
         else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println(" Потребуется дней : " + (int)(timeOfDelivery + 1));
+            System.out.println("Потребуется дней : " + (int)(timeOfDelivery + 1));
         }
         else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println(" Потребуется дней : " + (int) (timeOfDelivery + 2));
+            System.out.println("Потребуется дней : " + (int) (timeOfDelivery + 2));
         }
         else if (deliveryDistance > 100) {
             System.out.println("Доставки нет!");
@@ -67,70 +69,69 @@ public class Main {
 //        Task5
         System.out.println();
 
-        String season = null;
+        String season;
         String monthName;
         int monthNumber = 5;
-
-        if (monthNumber == 12 || monthNumber == 1 || monthNumber == 2) {
-            season = "Зима";
-        }
-        else if (monthNumber > 2 && monthNumber < 6) {
-            season = "Весна";
-        }
-        else if (monthNumber > 5 && monthNumber < 9) {
-            season = "Лето";
-        }
-        else if (monthNumber > 8 && monthNumber < 12) {
-            season = "Осень";
-        }
 
         switch (monthNumber) {
             case 1 :
                 monthName = "Январь";
-                System.out.printf("1-ый месяц %s это %s", monthName, season);
+                season = "Зима";
+                System.out.printf("1-ый месяц %s это %s\n", monthName, season);
                 break;
             case 2 :
                 monthName = "Февраль";
-                System.out.printf("2-ой месяц %s это %s", monthName, season);
+                season = "Зима";
+                System.out.printf("2-ой месяц %s это %s\n", monthName, season);
                 break;
             case 3 :
                 monthName = "Март";
-                System.out.printf("3-ий месяц %s это %s", monthName, season);
+                season = "Весна";
+                System.out.printf("3-ий месяц %s это %s\n", monthName, season);
                 break;
             case 4 :
                 monthName = "Апрель";
-                System.out.printf("4-ый месяц %s это %s", monthName, season);
+                season = "Весна";
+                System.out.printf("4-ый месяц %s это %s\n", monthName, season);
                 break;
             case 5 :
                 monthName = "Май";
-                System.out.printf("5-ый месяц %s это %s", monthName, season);
+                season = "Весна";
+                System.out.printf("5-ый месяц %s это %s\n", monthName, season);
                 break;
             case 6 :
                 monthName = "Июнь";
-                System.out.printf("6-ой месяц %s это %s", monthName, season);
+                season = "Лето";
+                System.out.printf("6-ой месяц %s это %s\n", monthName, season);
             case 7 :
                 monthName = "Июль";
-                System.out.printf("7-ой месяц %s это %s", monthName, season);
+                season = "Лето";
+                System.out.printf("7-ой месяц %s это %s\n", monthName, season);
             case 8 :
                 monthName = "Август";
-                System.out.printf("8-ой месяц %s это %s", monthName, season);
+                season = "Лето";
+                System.out.printf("8-ой месяц %s это %s\n", monthName, season);
             case 9 :
                 monthName = "Сентябрь";
-                System.out.printf("9-ый месяц %s это %s", monthName, season);
+                season = "Осень";
+                System.out.printf("9-ый месяц %s это %s\n", monthName, season);
             case 10 :
                 monthName = "Октябрь";
-                System.out.printf("10-ый месяц %s это %s", monthName, season);
+                season = "Осень";
+                System.out.printf("10-ый месяц %s это %s\n", monthName, season);
                 break;
             case 11 :
                 monthName = "Ноябрь";
-                System.out.printf("11-ый месяц %s это %s", monthName, season);
+                season = "Осень";
+                System.out.printf("11-ый месяц %s это %s\n", monthName, season);
                 break;
             case 12 :
                 monthName = "Декабрь";
-                System.out.printf("12-ый месяц %s это %s", monthName, season);
+                season = "Зима";
+                System.out.printf("12-ый месяц %s это %s\n", monthName, season);
                 break;
             default :
-                System.out.println("В году только 12 месяцев Увы!");
+                System.out.println("В году только 12 месяцев Увы!\n");
                 break;
         }
     }
